439493dbb5901a56b5611e21226e589f3da3a5f1
Vivek <vivekanandat92@gmail.com>
2024-04-19 15:15:09 +0530

    Commit

---
+++ src/main/java/com/vivek/quizapp/controller/QuizController.java
@@ -0,0 +1,37 @@
+package com.vivek.quizapp.controller;
+
+import com.vivek.quizapp.model.Question;
+import com.vivek.quizapp.model.QuestionWrapper;
+import com.vivek.quizapp.model.Response;
+import com.vivek.quizapp.service.QuizService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+// controller for quiz
+@RestController
+@RequestMapping("quiz")
+public class QuizController {
+
+    @Autowired
+    QuizService quizService;
+
+    @PostMapping("create")
+    public ResponseEntity<String> createQuiz(@RequestParam String category, @RequestParam int numQ, @RequestParam String title){
+        return quizService.createQuiz(category, numQ, title);
+    }
+    @GetMapping("get/{id}")
+    public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
+        return quizService.getQuizQuestions(id);
+    }
+
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+        return quizService.calculateResult(id, responses);
+    }
+
+
+}
